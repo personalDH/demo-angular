fd3f3f1d88d7d10df1ee05bd06df149378ad8ea0
Douglas <douglas@rsdata.com.br>
2018-07-18 16:37:05 -0300

    Implementacao da lista de paises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  title = 'Lista de países';
+
+  private url: string = "https://restcountries.eu/rest/v2/all";
+  public countries = "";
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit() {
+    this.carregarRegistros();
+  }
+
+  carregarRegistros() {
+    this.http.get(this.url).subscribe(
+      data => {
+        const response = (data as any);
+        console.log(response);
+        this.countries = response;
+      }, error => {
+        console.log(error);
+      }
+    )
+  }
 }
